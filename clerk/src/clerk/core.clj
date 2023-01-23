32cb7a6e26bad3d270172a8098bb1d4f8ab18491
Yannick Lehnhausen <yannick@lehnhausen.dev>
2023-01-23 08:29:59 +0100

    feat: add solution 2 for day 1

--- clerk/src/clerk/core.clj
+++ clerk/src/clerk/core.clj
@@ -4,9 +4,33 @@
 (clerk/serve! {:browse? true})
 
 ;; either call `clerk/show!` explicitly
-(clerk/show! "code/aoc_1/aoc_1.clj")
-
-(clerk/build! {:paths ["code/aoc_1/aoc_1.clj" "code/aoc_2/aoc_2.clj"] :bundle true :ssr true :out-path "public/build"})
+(clerk/build! {:paths ["code/day_01.clj"
+                       "code/day_02.clj"
+                       "code/day_03.clj"
+                       "code/day_03.clj"
+                       "code/day_04.clj"
+                       "code/day_05.clj"
+                       "code/day_06.clj"
+                       "code/day_07.clj"
+                       "code/day_08.clj"
+                       "code/day_09.clj"
+                       "code/day_10.clj"
+                       "code/day_11.clj"
+                       "code/day_12.clj"
+                       "code/day_13.clj"
+                       "code/day_14.clj"
+                       "code/day_15.clj"
+                       "code/day_16.clj"
+                       "code/day_17.clj"
+                       "code/day_18.clj"
+                       "code/day_19.clj"
+                       "code/day_20.clj"
+                       "code/day_21.clj"
+                       "code/day_22.clj"
+                       "code/day_23.clj"
+                       "code/day_24.clj"
+                       "code/day_25.clj"]
+               :bundle true :out-path "public/build"})
 
 ;; or let Clerk watch the given `:paths` for changes
 (clerk/serve! {:watch-paths ["code"]})
